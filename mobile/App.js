97f6897fc36a1e74a1ec938cab4bf3832137894f
kalliehuynh <kallie1@ualberta.ca>
2022-10-01 17:59:57 -0600

    added button

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,30 @@
 import React, { useState } from 'react'
-import { View, Text, Image, StyleSheet, FlatList, Alert } from 'react-native'
+import { View, Text, Image, StyleSheet, FlatList, Alert, Button } from 'react-native'
+import Main from './src/views/Main'
 
-import HomeView from './src/views/HomeView'
 
 const App = () => {
-  return <HomeView />
+  const [viewMain, setViewMain] = useState(false)
+
+  if (viewMain) {
+    return <Main />
+  }
+
+  return (
+      <View style={styles.container}>
+        <Text>Hello! How are you feeling today?</Text>
+        <Button onPress={() => {setViewMain(true)}} title='next'/>
+      </View>
+    )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30,
+  }
+})
 
 export default App
---
+++ mobile/src/views/Main.js
@@ -0,0 +1,18 @@
+// Main contains the main page with all of the features.
+
+import {
+    View,
+    Text
+} from 'react-native'
+
+const Main = () => {
+    return (
+        <View>
+            <Text>
+                main
+            </Text>
+        </View>
+    )
+}
+
+export default Main
