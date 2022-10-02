2c128980cdb6679184b2d7daad117cdcfa4320d1
Farel Lukas <farellukas@gmail.com>
2022-10-01 18:28:05 -0600

    Updated CalendarPicker

--- mobile/src/components/CalendarPicker.js
+++ mobile/src/components/CalendarPicker.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars'
 
 const CalendarPicker = () => {
+  const [markedDate, setMarkedDate] = useState({})
+
   return (
-    <Calendar />
+    <Calendar
+      enableSwipeMonths = {true}
+    />
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 50,
+    margin: 50
   }
 })
 
